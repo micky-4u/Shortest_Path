5b15b308b4b220e528764fe0f026697be0deb7ca
micky-4u <laryeamichael4u@gmail.com>
2021-11-23 06:28:33 +0000

    shortest Path

---
+++ src/Main/MainProgram.java
@@ -0,0 +1,75 @@
+package Main;
+
+import java.util.ArrayList;
+import Main.ShortestDistance;
+
+
+public class MainProgram {
+
+    static final int N = 100005;
+    static final int MAXI = 99999999;
+
+    public static void main(String[] args)
+    {
+        ShortestDistance SD = new ShortestDistance();
+        // Number of vertices and edges
+        int n = 25, m = 25;
+
+        @SuppressWarnings("unchecked")
+        ArrayList<ShortestDistance.Pair>[] gr = new ArrayList[N];
+
+        for(int i = 0; i < N; i++)
+        {
+            gr[i] = new ArrayList<ShortestDistance.Pair>();
+        }
+
+        // Function call to add edges
+        SD.add_edge(gr, 0, 1, 139);
+        SD.add_edge(gr, 1, 2, 100);
+        SD.add_edge(gr, 2, 3, 90);
+        SD.add_edge(gr, 3, 4, 120);
+        SD.add_edge(gr, 4, 5, 101);
+        SD.add_edge(gr, 5, 6, 118);
+        SD.add_edge(gr, 6, 7, 93);
+        SD.add_edge(gr, 7, 8, 90);
+        SD.add_edge(gr, 8, 9, 79);
+        SD.add_edge(gr, 8, 12, 50);
+        SD.add_edge(gr, 12, 13, 64);
+        SD.add_edge(gr, 13, 14, 196);
+        SD.add_edge(gr, 14, 15, 67);
+        SD.add_edge(gr, 15, 16, 183);
+        SD.add_edge(gr, 16, 17, 95);
+        SD.add_edge(gr, 17, 18, 61);
+        SD.add_edge(gr, 6, 21, 207);
+        SD.add_edge(gr, 19, 20, 46);
+        SD.add_edge(gr, 20, 21, 40);
+        SD.add_edge(gr, 21, 22, 264);
+        SD.add_edge(gr, 22, 23, 100);
+        SD.add_edge(gr, 23, 24, 37);
+
+
+
+
+
+
+
+
+
+
+        // Number of good nodes
+        int k = 20;
+
+        int[] a = new int[N],
+                vis = new int[N],
+                dist = new int[N];
+
+        // To keep good vertices
+        for( int i = 0; i<25 ; i++){
+            a[i] = 1;
+        }
+
+        System.out.println(SD.minDistance(
+                gr, n, dist, vis, a, k));
+    }
+
+}
---
+++ src/Main/ShortestDistance.java
@@ -0,0 +1,146 @@
+package Main;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+public class ShortestDistance {
+    // Java program to find the shortest pairwise
+// distance between any two different good nodes.
+
+
+        static class Pair
+        {
+            int source, destination;
+
+            public Pair(int source, int destination)
+            {
+                this.source = source;
+                this.destination = destination;
+            }
+
+            public Pair()
+            {}
+        }
+
+        static final int N = 100005;
+        static final int MAXI = 99999999;
+
+        // Function to add edges
+        static void add_edge(ArrayList<Pair> gr[],
+                             int x, int y, int weight)
+        {
+            gr[x].add(new Pair(y, weight));
+            gr[y].add(new Pair(x, weight));
+        }
+
+        // Function to find the shortest
+// distance between any pair of
+// two different good nodes
+        static int minDistance(ArrayList<Pair> gr[], int n,
+                               int dist[], int vis[],
+                               int a[], int k)
+        {
+
+            // Keeps minimum element on top
+            PriorityQueue<Pair> q = new PriorityQueue<>(
+                    new Comparator<Pair>()
+                    {
+                        public int compare(Pair p1, Pair p2)
+                        {
+                            if (p1.source == p2.source)
+                            {
+                                return p1.destination - p2.destination;
+                            }
+                            return p1.source - p2.source;
+                        }
+                    });
+
+            // To keep required answer
+            int ans = MAXI;
+
+            for(int i = 1; i <= n; i++)
+            {
+
+                // If it is not good vertex
+                if (a[i] == 0)
+                    continue;
+
+                // Keep all vertices not visited
+                // and distance as MAXI
+                for(int j = 1; j <= n; j++)
+                {
+                    dist[j] = MAXI;
+                    vis[j] = 0;
+                }
+
+                // Distance from ith vertex
+                // to ith is zero
+                dist[i] = 0;
+
+                // Make queue empty
+                while (!q.isEmpty())
+                    q.poll();
+
+                // Push the ith vertex
+                q.add(new Pair(0, i));
+
+                // Count the good vertices
+                int good = 0;
+
+                while (!q.isEmpty())
+                {
+
+                    // Take the top element
+                    int v = q.peek().destination;
+
+                    // Remove it
+                    q.poll();
+
+                    // If it is already visited
+                    if (vis[v] != 0)
+                        continue;
+                    vis[v] = 1;
+
+                    // Count good vertices
+                    good += a[v];
+
+                    // If distance from vth vertex
+                    // is greater than ans
+                    if (dist[v] > ans)
+                        break;
+
+                    // If two good vertices are found
+                    if (good == 2 && a[v] != 0)
+                    {
+                        ans = Math.min(ans, dist[v]);
+                        break;
+                    }
+
+                    // Go to all adjacent vertices
+                    for(int j = 0; j < gr[v].size(); j++)
+                    {
+                        int to = gr[v].get(j).source;
+                        int weight = gr[v].get(j).destination;
+
+                        // If distance is less
+                        if (dist[v] + weight < dist[to])
+                        {
+                            dist[to] = dist[v] + weight;
+                            q.add(new Pair(dist[to], to));
+                        }
+                    }
+                }
+            }
+
+            // Return the required answer
+            return ans;
+        }
+
+        // Driver code
+
+
+
+}
