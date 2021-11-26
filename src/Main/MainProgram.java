3f155fbb38200a0956bd5c5acc4e30799786e620
micky-4u <laryeamichael4u@gmail.com>
2021-11-26 13:17:58 +0000

    project files

--- src/Main/MainProgram.java
+++ src/Main/MainProgram.java
@@ -43,6 +43,29 @@ public class MainProgram {
         SD.add_edge(weighted_edge, 14, 15, 117);
         SD.add_edge(weighted_edge, 15, 16, 103);
         SD.add_edge(weighted_edge, 16, 17, 109);
+        SD.add_edge(weighted_edge, 18, 19, 370);
+        SD.add_edge(weighted_edge, 19, 49, 348);
+        SD.add_edge(weighted_edge, 19, 20, 216);
+        SD.add_edge(weighted_edge, 20, 21, 150);
+        SD.add_edge(weighted_edge, 21, 22, 132);
+        SD.add_edge(weighted_edge, 22, 23, 138);
+        SD.add_edge(weighted_edge, 23, 24, 60);
+        SD.add_edge(weighted_edge, 24, 2, 70);
+        SD.add_edge(weighted_edge, 3, 25, 280);
+        SD.add_edge(weighted_edge, 21, 25, 190);
+        SD.add_edge(weighted_edge, 25, 26, 120);
+        SD.add_edge(weighted_edge, 26, 27, 170);
+        SD.add_edge(weighted_edge, 27, 28, 160);
+        SD.add_edge(weighted_edge, 28, 11, 120);
+        SD.add_edge(weighted_edge, 5, 26, 275);
+        SD.add_edge(weighted_edge, 30, 29, 120);
+        SD.add_edge(weighted_edge, 29, 27, 134);
+        SD.add_edge(weighted_edge, 7, 32, 202);
+        SD.add_edge(weighted_edge, 32, 31, 60);
+        SD.add_edge(weighted_edge, 31, 28, 217);
+        SD.add_edge(weighted_edge, 27, 51, 191);
+        SD.add_edge(weighted_edge, 51, 50, 154);
+        SD.add_edge(weighted_edge, 50, 13, 138);
 
 
 
